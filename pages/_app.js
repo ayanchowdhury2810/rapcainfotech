6071e5f1639dc7bef39d8460afd57117782b7f09
Ayan <ayan123chowdhury@gmail.com>
2022-09-09 09:06:09 +0530

    Pushed

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,30 @@ import '../public/assets/css/vendor/sal.css';
 import '../public/assets/css/vendor/magnific-popup.css';
 import '../public/assets/css/vendor/green-audio-player.min.css';
 import '../public/assets/css/App.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script src="assets/js/vendor/jquery-3.6.0.min.js" />
+      <Script src="assets/js/vendor/bootstrap.min.js" />
+      <Script src="assets/js/vendor/isotope.pkgd.min.js" />
+      <Script src="assets/js/vendor/imagesloaded.pkgd.min.js" />
+      <Script src="assets/js/vendor/waypoints.min.js" />
+      <Script src="assets/js/vendor/counterup.js" />
+      <Script src="assets/js/vendor/slick.min.js" />
+      <Script src="assets/js/vendor/sal.js" />
+      <Script src="assets/js/vendor/jquery.magnific-popup.min.js" />
+      <Script src="assets/js/vendor/js.cookie.js" />
+      <Script src="assets/js/vendor/jquery.style.switcher.js" />
+      <Script src="assets/js/vendor/jquery.countdown.min.js" />
+      <Script src="assets/js/vendor/tilt.js" />
+      <Script src="assets/js/vendor/green-audio-player.min.js" />
+      <Script src="assets/js/vendor/jquery.nav.js" />
+      <Script src="public/assets/js/app.js" />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
